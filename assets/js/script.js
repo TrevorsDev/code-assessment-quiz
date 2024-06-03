0b0c1f859d3f440cfaeec92a9d9190c86ca73cd6
Trevor Johnson <trevor9j@protonmail.com>
2024-06-03 14:37:31 -0700

    added countdown timer, javascript display question functionality, and display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,37 @@
+const questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
 const startBtn = document.getElementById('start');
 const timerEl = document.getElementById('timer');
 let timeInterval;
@@ -10,3 +44,27 @@ startBtn.addEventListener('click', () => {
   timerEl.textContent = time
   displayQuestion();
 })
+
+function countdown() {
+  time--;
+  timerEl.textContent = time;
+  if (time <= 0) {
+    clearInterval(timeInterval);
+    timerEl.textContent = 'Time is up!';
+    endGame();
+  }
+}
+
+function displayQuestion() {
+  //questions will be displayed here
+  let currentQuestion = questions[index];
+  document.getElementById('question').innerHTML = currentQuestion.title;
+  let answerEl = document.getElementById('answers');
+  answerEl.innerHTML = '';
+  currentQuestion.choices.forEach((choice, i) => {
+    const btn = document.createElement('button');
+    btn.textContent = choice;
+    btn.onclick = checkAnswer;
+    answerEl.append(btn);
+  })
+}
